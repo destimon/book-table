849071cf0ad42d0164ec5854434730e553c88138
destimon <destaom@gmail.com>
2020-06-28 20:10:15 +0300

    fix navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-// import '../../assets/main.scss';
 
 const Navbar = () => {
   return (
     <nav className="blue-grey darken-4" style={{marginBottom: '5%', paddingLeft: '3%', paddingRight: '3%'}}>
       <div className="nav-wrapper">
-        <Link to="/" className="brand-logo">BookTable</Link>
+        <Link to="/" className="brand-logo left">BookTable</Link>
         <ul id="nav-mobile" className="right">
+          <li><Link to="/profile">Profile</Link></li>
           <li><Link to="/about">About</Link></li>
         </ul>
       </div>
