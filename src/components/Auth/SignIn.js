680912bdc3703ddd8b92a5fa92f1b0918cab2a11
destimon <destaom@gmail.com>
2020-06-30 02:13:18 +0300

    Formik on sign in

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -1,58 +1,81 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 import { signInUser } from '../../store/actions/userAction'
 import PropTypes from 'prop-types'
+import { Formik } from 'formik';
 
 const SignIn = ({ signInUser, history }) => {
-  const [user, setUser] = useState({
-    username: '',
-    password: '',
-  });
-
-  const handleChange = e => {
-    setUser({
-      ...user,
-      [e.target.name]: e.target.value,
-    });
+  const validateForm = (values) => {
+    const errors = {};
+    if (!values.username) {
+      errors.username = 'Username required';
+    } else if (values.username.length > 10) {
+      errors.username = 'Username too long';
+    }
+    return errors;
   }
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    signInUser(user);
+  const submitForm = (values, {setSubmitting}) => {
+    signInUser({
+      username: values.username,
+      password: values.password
+    });
+    setSubmitting(false);
     history.push('/');
   }
 
   return (
-    <div className="row">
-      <div className="col l6">
-        <form className="left-align" onSubmit={handleSubmit}>
-          <div className="input-field">
-            <input 
-              id="username" 
-              type="text" 
-              name="username"
-              className="validate"
-              value={user.username}
-              onChange={handleChange}
-            />
-            <label htmlFor="username">Username</label>
+    <Formik
+      initialValues={{ username: '', password: '' }}
+      validate={validateForm}
+      onSubmit={submitForm}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+        <div className="row">
+          <div className="col l6">
+            <form className="left-align" onSubmit={handleSubmit}>
+              <div className="input-field">
+                <input
+                  id="username"
+                  type="text"
+                  className="validate"
+                  name="username"
+                  onChange={handleChange}
+                  value={values.username}
+                />
+                <label htmlFor="username">Username</label>
+                {errors.username && touched.username && errors.username}
+              </div>
+              <div className="input-field">
+                <input
+                  id="password"
+                  type="password"
+                  className="validate"
+                  name="password"
+                  onChange={handleChange}
+                  value={values.password}
+                />
+                <label htmlFor="password">Password</label>
+                {errors.password && touched.password && errors.password}
+              </div>
+                <input
+                  value="Sign in" 
+                  type='submit'
+                  className="right btn blue-grey darken-4"
+                  disabled={isSubmitting}
+                />
+            </form>
           </div>
-
-          <div className="input-field">
-            <input
-              id="password" 
-              type='password'
-              name='password'
-              className="validate"
-              value={user.password}
-              onChange={handleChange}
-            />
-            <label htmlFor="password">Password</label>
-          </div>
-          <input className="right btn blue-grey darken-4" value="Sign in" type='submit'/>
-        </form>
-      </div>
-    </div>
+        </div>
+      )}
+    </Formik>
   )
 }
 
