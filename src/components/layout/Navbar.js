e0b3708be5bf187ef24485a2e2761a731c48ca5d
destimon <destaom@gmail.com>
2020-07-01 00:12:36 +0300

    Navbar profile and auth button

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,15 +2,29 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const {
+    user: {
+      userProfileLoading,
+      isAuthenticated
+    }
+  } = props;
+
   return (
     <nav className="blue-grey darken-4" style={{marginBottom: '5%', paddingLeft: '3%', paddingRight: '3%'}}>
       <div className="nav-wrapper">
         <Link to="/" className="brand-logo left">BookTable</Link>
         <ul id="nav-mobile" className="right">
-          <li><Link to="/profile">
-            { 'Profile' }
-          </Link></li>
+          <li>
+            {
+              !userProfileLoading && (
+                <Link to="/profile">
+                { (isAuthenticated) ? ( 'Profile' ) : 'Sign up' }
+                </Link>
+              )
+            }
+
+          </li>
           <li><Link to="/about">About</Link></li>
         </ul>
       </div>
