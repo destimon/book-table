4ff9b836635d7686a6273da60aa96b715c121556
destimon <destaom@gmail.com>
2020-07-08 01:14:47 +0300

    Refactor BookItem

--- client/src/components/Books/BookItem.js
+++ client/src/components/Books/BookItem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import PropTypes from 'prop-types'
 import { useHistory } from "react-router-dom";
 import '../../assets/main.scss';
@@ -6,38 +6,34 @@ import '../../assets/main.scss';
 const BookItem = ({ book }) => {
   const history = useHistory();
 
-  const showBook = () => {
+  const showBook = useCallback(() => {
     history.push(`/books/${book.id}`)
-  }
+  }, [book, history]);
 
   return (
-      <li className="collection-item book-item" onClick={showBook}>
-        <div className="row">
-          <div className="col s6 m4 l2 xl2">
-            <img
-              alt="book" 
-              className="book-image" 
-              src={book.thumbnail || '/noimage.jpeg'} 
-            />
-              <blockquote>
-                <div>
-                  {
-                    book.authors &&
-                    book.authors.map(author => (
-                      (author)
-                    ))
-                  }
-                </div>
-              </blockquote>
-          </div>
-          <div className="col s6 m8 l10 xl10">
-            <h5>{book.title}</h5>
-            <div className="divider"></div>
-            <p>Description...</p>
-            <small>{book.publishedDate}</small>
-          </div>
+    <li className="collection-item book-item" onClick={showBook}>
+      <div className="row">
+        <div className="col s6 m4 l2 xl2">
+          <img
+            alt="book" 
+            className="book-image" 
+            src={book.thumbnail || '/noimage.jpeg'} 
+          />
+          <blockquote>
+            {
+              book.authors &&
+              book.authors.map((author, index) => ( <p key={index}>{author}</p> ))
+            }
+          </blockquote>
         </div>
-      </li>
+        <div className="col s6 m8 l10 xl10">
+          <h5>{book.title}</h5>
+          <div className="divider"></div>
+          <p>Description...</p>
+          <small>{book.publishedDate}</small>
+        </div>
+      </div>
+    </li>
   )
 }
 
