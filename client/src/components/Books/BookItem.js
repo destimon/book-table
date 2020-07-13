837d8ed6465d9699cd06a35241b6d3c799729a12
destimon <destaom@gmail.com>
2020-07-13 04:25:56 +0300

    Book title useMemo

--- client/src/components/Books/BookItem.js
+++ client/src/components/Books/BookItem.js
@@ -15,6 +15,10 @@ const BookItem = ({ book }) => {
     _.truncate(book.description, { 'length': 300 })
   ), [book.description]);
 
+    const bookTitle = useMemo(() => (
+      _.truncate(book.title, { 'length': 100 })
+    ), [book.title])
+
   return (
     <li className="collection-item book-item" onClick={showBook}>
       <div className="row">
@@ -27,12 +31,12 @@ const BookItem = ({ book }) => {
           <blockquote>
             {
               book.authors &&
-              book.authors.map((author, index) => ( <p key={index}>{author}</p> ))
+              book.authors.map((author, index) => (<p key={index}>{author}</p> ))
             }
           </blockquote>
         </div>
         <div className="col s6 m8 l10 xl10">
-          <h5>{book.title}</h5>
+          <h5>{bookTitle}</h5>
           <div className="divider"></div>
           <p>{bookDescription}</p>
           <small>{book.publishedDate}</small>
